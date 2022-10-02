413ff7fcf7b893eaf9ff7e2bc277ea6b0d0ed860
Mehmet Can Kahveci <111227853+memocankahveci@users.noreply.github.com>
2022-10-02 19:18:53 +0300

    Add files via upload

---
+++ keltos.js
@@ -0,0 +1,9 @@
+var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++) {
+    if(!a[i].onclick && a[i].getAttribute("target") != "_blank") {
+        a[i].onclick=function() {
+                window.location=this.getAttribute("href");
+                return false; 
+        }
+    }
+}
