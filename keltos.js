8c21943544272e16e22905cf259d8c3070c8898f
Mehmet Can Kahveci <111227853+memocankahveci@users.noreply.github.com>
2022-10-02 19:24:04 +0300

    Delete keltos.js

--- keltos.js
+++
@@ -1,9 +0,0 @@
-var a=document.getElementsByTagName("a");
-for(var i=0;i<a.length;i++) {
-    if(!a[i].onclick && a[i].getAttribute("target") != "_blank") {
-        a[i].onclick=function() {
-                window.location=this.getAttribute("href");
-                return false; 
-        }
-    }
-}
