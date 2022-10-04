adfb8bed70fca9fd689532cfeb2ced31241dadd6
Mehmet Can Kahveci <111227853+memocankahveci@users.noreply.github.com>
2022-10-04 23:20:47 +0300

    Update Wed.js

--- DailyProgram/Wed.js
+++ DailyProgram/Wed.js
@@ -3,21 +3,21 @@ var time = today.getHours() * 100 + today.getMinutes();
 var greet;
 
 if (time > 1430) {
-  greet = 'Current Class: Period X/End of School';
+  greet = 'Next Class: Period X/End of School';
 } else if (time > 1310) {
-  greet = 'Current Class: Next Lesson: BA203/Literature';
+  greet = 'Next Class: Next Lesson: BA203/Literature';
 } else if (time > 12) {
-  greet = 'Current Class: Next Lesson: BA203/Communication';
+  greet = 'Next Class: Next Lesson: BA203/Communication';
 } else if (time > 1030) {
-    greet = 'Current Class: Next Lesson: GYM/PhysEd';
+    greet = 'Next Class: Next Lesson: GYM/PhysEd';
 } else if (time > 950) {
-    greet = 'Current Class: Next Lesson: BA201/Language';
+    greet = 'Next Class: Next Lesson: BA201/Language';
 } else if (time > 845) {
-    greet = 'Current Class: Next Lesson: BA101/Literature';
+    greet = 'Next Class: Next Lesson: BA101/Literature';
 } else if (time > 805) {
-    greet = 'Current Class: Next Lesson: BA303/Language';
+    greet = 'Next Class: Next Lesson: BA303/Language';
 } else if (time > 700) {
-    greet = 'Current Class: Next Lesson: YD/German,French,Spanish';
+    greet = 'Next Class: Next Lesson: YD/German,French,Spanish';
 } else {
   greet = 'Something Wrong Dawg.';
 }
