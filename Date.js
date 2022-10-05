a77541f65e9c00cad1ea5916d8f41a23cb65c642
Mehmet Can Kahveci <111227853+memocankahveci@users.noreply.github.com>
2022-10-05 21:28:10 +0300

    Create Date.js

---
+++ Date.js
@@ -0,0 +1,8 @@
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+ 
+document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
