edf542c0ed3f7cdb068d6ee6b34710c1b21aee9f
Mehmet Can Kahveci <96494651+mehmetcankahveci@users.noreply.github.com>
2022-10-10 19:43:00 +0300

    Delete DailyProgram directory

--- DailyProgram/Weekend.js
+++
@@ -1,9 +0,0 @@
-var today = new Date();
-var time = today.getHours() * 100 + today.getMinutes();
-var greet;
-
-if (time >= 000) {
-  greet = 'Have a Nice Weekend!';
-}
-var show = document.getElementById('kelkelkel');
-show.textContent = greet;
