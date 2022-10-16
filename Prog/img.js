7399c123df19ecb2ab2b2014b3f8ca6807c8d4a8
Mehmet Can Kahveci <111227853+memocankahveci@users.noreply.github.com>
2022-10-16 13:24:00 +0300

    Update img.js

--- Prog/img.js
+++ Prog/img.js
@@ -16,5 +16,5 @@ else if (day == 5) {
     document.write("<img src='Prog/Prog.png'>")
 }
 else {
-    document.write("<img src='Prog/Prog.png'>")
+    document.write("<img src='Prog/rickroll.gif'>")
 }
