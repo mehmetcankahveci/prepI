af3b79f17f9a22ccd22c750243debf88688a245d
Mehmet Can Kahveci <111227853+memocankahveci@users.noreply.github.com>
2022-10-16 13:13:19 +0300

    Update img.js

--- Prog/img.js
+++ Prog/img.js
@@ -1,20 +1,20 @@
 var today = new Date();
 var day = today.getDay()
 if (day == 1){
-    document.write("<img src='Prog/Mon.png'>")
+    document.write("<img src='Prog/Prog.png'>")
 }
 else if (day == 2) {
-    document.write("<img src='Prog/Tue.png'>")
+    document.write("<img src='Prog/Prog.png'>")
 }
 else if (day == 3) {
-    document.write("<img src='Prog/Wed.png'>")
+    document.write("<img src='Prog/Prog.png'>")
 }
 else if (day == 4) {
-    document.write("<img src='Prog/Thu.png'>")
+    document.write("<img src='Prog/Prog.png'>")
 }
 else if (day == 5) {
-    document.write("<img src='Prog/Fri.png'>")
+    document.write("<img src='Prog/Prog.png'>")
 }
 else {
-    document.write("<img src='Prog/rickroll.gif'>")
+    document.write("<img src='Prog/Prog.gif'>")
 }
