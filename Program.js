e0c3ebc316259be091c0e24ab50beee7564ae336
Mehmet Can Kahveci <96494651+mehmetcankahveci@users.noreply.github.com>
2022-10-30 12:52:58 +0300

    Update Program.js

--- Program.js
+++ Program.js
@@ -3,13 +3,13 @@ var day = today.getDay()
 var time = today.getHours() * 100 + today.getMinutes();
 var greet;
 if (day == 1) {
-    if (time > 1415) {
+    if (time > 1439) {
     greet = 'Next Class: Period X/End of School';
-    } else if (time > 1230) {
+    } else if (time > 1240) {
         greet = 'YD/German, French, Spanish';  
-    } else if (time > 1150) {
+    } else if (time > 1155) {
     greet = 'Next Class: BA104/Literature';
-    } else if (time > 950) {
+    } else if (time > 945) {
         greet = 'Next Class: BO210/Turkce';
     } else if (time > 845) {
         greet = 'Next Class: BA203/Language';
@@ -22,57 +22,57 @@ if (day == 1) {
     }
 }
 else if (day == 2) {
-    if (time > 1430) {
+    if (time > 1439) {
         greet = 'Next Class: Period X/End of School';
-    } else if (time > 1310) {
+    } else if (time > 1325) {
         greet = 'Next Class: : BO210/Turkce';
-    } else if (time > 1200) {
+    } else if (time > 1155) {
         greet = 'Next Class: : BA201/Guidance';
     } else if (time > 1030) {
         greet = 'Next Class: : BA201/Language';
-    } else if (time > 950) {
+    } else if (time > 945) {
         greet = 'Next Class: : BA202/Communication';
     } else if (time > 845) {
         greet = 'Next Class: EK29/CS';
     } else if (time > 805) {
         greet = 'Next Class: : BA101/Literature';
-    } else if (time > 700) {
+    } else if (time > 500) {
         greet = 'Next Class: : YD/German,French,Spanish';
     } else {
   greet = 'Daily Program will be available here soon.';
 } 
 }
 else if (day == 3) {
-    if (time > 1430) {
+    if (time > 1439) {
         greet = 'Next Class: Period X/End of School';
-    } else if (time > 1310) {
+    } else if (time > 1325) {
         greet = 'Next Class: BA203/Literature';
-    } else if (time > 1200) {
+    } else if (time > 1155) {
         greet = 'Next Class: BA203/Communication';
     } else if (time > 1030) {
         greet = 'Next Class: GYM/PhysEd';
-    } else if (time > 950) {
+    } else if (time > 945) {
         greet = 'Next Class: BA201/Language';
     } else if (time > 845) {
         greet = 'Next Class: : BA101/Literature';
     } else if (time > 805) {
         greet = 'Next Class: : BA303/Language';
-    } else if (time > 700) {
+    } else if (time > 500) {
         greet = 'Next Class: : YD/German,French,Spanish';
     } else {
         greet = 'Daily Program will be available here soon.';
     }
 }
 else if (day == 4) {
-    if (time > 1415) {
+    if (time > 1439) {
         greet = 'Next Class: Period X/End of School';
-      } else if (time > 1230) {
+      } else if (time > 1240) {
           greet = 'Next Class: Art';  
-      } else if (time > 1150) {
+      } else if (time > 1155) {
         greet = 'Next Class: BA203/Communication';
       } else if (time > 1030) {
           greet = 'Next Class: MA303/Maths';
-      } else if (time > 950) {
+      } else if (time > 945) {
           greet = 'Next Class: BO210/Turkce';
       } else if (time > 845) {
           greet = 'Next Class: YD/German, French, Spanish';
@@ -85,13 +85,13 @@ else if (day == 4) {
       }
 }
 else if (day == 5) {
-    if (time > 1415) {
+    if (time > 1439) {
         greet = 'Next Class: Period X/End of School';
-      } else if (time > 1230) {
+      } else if (time > 1240) {
           greet = 'BO210/Turkce';  
-      } else if (time > 1150) {
+      } else if (time > 1155) {
         greet = 'Next Class: BA202/Language';
-      } else if (time > 950) {
+      } else if (time > 945) {
           greet = 'Next Class: BA003/Literature';
       } else if (time > 845) {
           greet = 'Next Class: BA201/Communication';
@@ -103,15 +103,11 @@ else if (day == 5) {
         greet = 'Daily Program will be available here soon.';
       }
 }
-else if (day == 6) {
-    if (time >= 000) {
-        greet = 'Have a Nice Weekend!';
-      }
-}
 else {
     if (time >= 000) {
         greet = 'Have a Nice Weekend!';
       }
+    
 }
 
 var show = document.getElementById('kelkelkel');
