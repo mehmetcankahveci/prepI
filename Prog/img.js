aaf066e261e56f2cc89345ef0294ae79cbba6f3f
MG95 <kayayetkin@hotmail.com>
2022-11-09 15:05:47 +0300

    Why is this so inefficient???
    easy fix

--- Prog/img.js
+++ Prog/img.js
@@ -1,18 +1,6 @@
 var today = new Date();
 var day = today.getDay()
-if (day == 1){
-    document.write("<img src='Prog/Prog.png'>")
-}
-else if (day == 2) {
-    document.write("<img src='Prog/Prog.png'>")
-}
-else if (day == 3) {
-    document.write("<img src='Prog/Prog.png'>")
-}
-else if (day == 4) {
-    document.write("<img src='Prog/Prog.png'>")
-}
-else if (day == 5) {
+if (1 <= day <= 5){
     document.write("<img src='Prog/Prog.png'>")
 }
 else {
