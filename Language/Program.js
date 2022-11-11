f61abb9959d7801c908161e351e0b37f7dea21d2
MG95 <kayayetkin@hotmail.com>
2022-11-11 10:48:50 +0300

    revert timer, a bit more lamer approach to refresh, it now refreshes on focus change

--- Language/Program.js
+++ Language/Program.js
@@ -1,11 +1,11 @@
 var today = new Date();
 var day = today.getDay()
 var time = today.getHours() * 100 + today.getMinutes();
-let greet = 'Daily Program will be available here soon.';
 import lessons from './lessons.json' assert {type: 'json'}
 
-
 function getClass() {
+    let greet = 'Daily Program will be available here soon.';
+
     if (day <= 5 && time <= 1439) {
         lessons[day - 1].some(lesson => {
             if (time > lesson['time']) {
@@ -23,12 +23,12 @@ function getClass() {
 
     var show = document.getElementById('kelkelkel');
     show.textContent = greet;
-    var count = 60
-    setInterval(function () {
-        count--;
-        document.getElementById('counter').textContent = 'Next refresh in: ' + count + ' seconds';
-    }, 1000);
 }
 
 getClass();
-setInterval(function () {getClass();}, 60 * 1000);
+
+document.addEventListener("visibilitychange", (event) => {
+    if (document.visibilityState == "visible") {
+        getClass();
+    }
+});
