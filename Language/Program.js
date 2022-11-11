f77ba672b1c7734bc7da2bdcbf99e671227e96a0
MG95 <kayayetkin@hotmail.com>
2022-11-11 09:31:15 +0300

    add auto update

--- Language/Program.js
+++ Language/Program.js
@@ -1,24 +1,30 @@
 var today = new Date();
 var day = today.getDay()
 var time = today.getHours() * 100 + today.getMinutes();
-var greet = 'Daily Program will be available here soon.';
+let greet = 'Daily Program will be available here soon.';
 import lessons from './lessons.json' assert {type: 'json'}
 
 
-if (day <= 5 && time <= 1439) {
-    lessons[day - 1].some(lesson => {
-        if (time > lesson['time']) {
-            greet = 'Next Class: ' + lesson['name']
-            return true;
-        }
-    });
-}
-else if (time > 1439) {
-    greet = 'Next Class: Period X/End of School'
-}
-else {
-    greet = 'Have a Nice Weekend!'
+function getClass() {
+    if (day <= 5 && time <= 1439) {
+        lessons[day - 1].some(lesson => {
+            if (time > lesson['time']) {
+                greet = 'Next Class: ' + lesson['name']
+                return true;
+            }
+        });
+    }
+    else if (time > 1439) {
+        greet = 'Next Class: Period X/End of School'
+    }
+    else {
+        greet = 'Have a Nice Weekend!'
+    }
+
+    var show = document.getElementById('kelkelkel');
+    show.textContent = greet;
 }
 
-var show = document.getElementById('kelkelkel');
-show.textContent = greet;
+getClass();
+setInterval(function () {getClass();}, 60 * 1000);
+
