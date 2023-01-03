68c1937efe5deea961eb1472e8e4d994e9fb77be
Mehmet Can <poop@poop.com>
2023-01-03 20:15:23 +0300

    Redesign

---
+++ JS/Date.js
@@ -0,0 +1,7 @@
+var today = new Date();
+var day = today.getDay();
+var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
+var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+var dateTime = date
+ 
+document.getElementById("displayDateTime").innerHTML = dateTime + ' <span> ' + daylist[day];
