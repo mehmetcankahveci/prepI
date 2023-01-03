a602a9247261cb6041a1af7cce89014c18c4cd31
Mehmet Can Kahveci <mehmetcankahveci@users.noreply.github.com>
2023-01-03 20:33:57 +0300

    Redesign

--- Prog/img.js
+++
@@ -1,8 +0,0 @@
-var today = new Date();
-var day = today.getDay()
-if (1 <= day <= 5){
-    document.write("<img src='Prog/Prog.png'>")
-}
-else {
-    document.write("<img src='Prog/rickroll.gif'>")
-}
