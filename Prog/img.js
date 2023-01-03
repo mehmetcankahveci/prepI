55e8329048ce4785dc698b57f070f901fdfa9d05
Mehmet Can <poop@poop.com>
2023-01-03 20:22:07 +0300

    Revert "Redesign"
    This reverts commit 68c1937efe5deea961eb1472e8e4d994e9fb77be.

---
+++ Prog/img.js
@@ -0,0 +1,8 @@
+var today = new Date();
+var day = today.getDay()
+if (1 <= day <= 5){
+    document.write("<img src='Prog/Prog.png'>")
+}
+else {
+    document.write("<img src='Prog/rickroll.gif'>")
+}
